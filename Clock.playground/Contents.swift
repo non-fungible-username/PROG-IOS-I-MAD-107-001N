e0257d6ce6dd6b9a4ffb6cb671aa07d8e4422790
Danny Amesquita <105797373+non-fungible-username@users.noreply.github.com>
2022-09-23 12:45:22 -0500

    commit

--- Clock.playground/Contents.swift
+++ Clock.playground/Contents.swift
@@ -3,15 +3,18 @@
 import UIKit
  
 func showTime() {
+     // Create a loop that repeats every second
      Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
      timer in
+         // Create variables for current time, hour, minute, and second
          var currentDateTime = Date()
          var hour = (Calendar.current.component(.hour, from: currentDateTime))
          var min = (Calendar.current.component(.minute, from: currentDateTime))
          var sec = (Calendar.current.component(.second, from: currentDateTime))
+         // Print the current time
          print("\(hour):\(min):\(sec)")
      }
  }
-
+// Run the clock
 showTime()
 
