af26a1bfde074638518ad3bc9ca6c57ca6ffa969
Danny Amesquita <105797373+non-fungible-username@users.noreply.github.com>
2022-10-14 12:32:07 -0500

    commit
    added functions for changing model and color

--- Build My Prius DannyA.playground/Contents.swift
+++ Build My Prius DannyA.playground/Contents.swift
@@ -3,6 +3,7 @@
 
 import UIKit
 
+// Enums for Models, Colors, Packages, and Accessories used to set parameters for Prius class variables and have string values for their correct name
 enum Models: String {
     case LEco = "L Eco"
     case LE = "LE"
@@ -68,54 +69,114 @@ enum Accessories: String {
     case UniversalTabletHolder = "Universal Tablet Holder"
 }
 
+// Pruis class models selecting, removing, and displaying options for a Prius car
 class Prius {
+    // Declare variables for the car using enums as parameters
     var model: Models
     var color: Colors
+    // Declare package and accessory variables as arrays with empty values so no initial init required
     var package: [Packages] = []
     var accessory: [Accessories] = []
     
+    // Initialize the model and color
     init(model: Models, color: Colors) {
         self.model = model
         self.color = color
     }
 
+    // Shows current car option selections
     func showSelections() {
-        print("Your model selection is \(self.model.rawValue)")
-        print("Your color selection is \(self.color.rawValue)")
+        print("Your selected model: \(self.model.rawValue)")
+        print("Your selected color: \(self.color.rawValue)")
         
+        // Checks if package array has at least one entry
         if self.package.count > 0 {
             print("Your packages selections include: ", terminator: "")
             for choice in self.package {
+                // prints chosen package selections and displays with correct grammar
                 if choice == self.package[0] {
                     print("\(choice.rawValue)", terminator: "")
                 } else {
                     print(", \(choice.rawValue)", terminator: "")
                 }
             }
-        } else {
-            print("No packages have been selected", terminator: "")
+        } else { // prints message if package array is empty
+            print("Your packages selections include: none", terminator: "")
         }
         
+        // Checks if accessory array has at least one entry
         if self.accessory.count > 0 {
             print("\nYour accessories selections include: ", terminator: "")
             for choice in self.accessory {
+                // prints chosen accessory selections and displays with correct grammar
                 if choice == self.accessory[0] {
                     print("\(choice.rawValue)", terminator: "")
                 } else {
                     print(", \(choice.rawValue)", terminator: "")
                 }
             }
-        } else {
-            print("\nNo accessories have been selected")
+        } else { // prints message if accessory array is empty
+            print("\nYour accessories selections include: none", terminator: "")
         }
         print("\n")
     }
     
+    // Changes the selected model
+    func changeModel(new_model: Models) {
+        if self.model != new_model {
+            self.model = new_model
+            print("New model: \(new_model.rawValue)\n")
+        } else { // catch if model already chosen
+            print("Error: \(new_model.rawValue) was already chosen\n")
+        }
+    }
+    
+    // Changes the selected color
+    func changeColor(new_color: Colors) {
+        if self.color != new_color {
+            self.color = new_color
+            print("New color: \(new_color.rawValue)\n")
+        } else { // catch if color already chosen
+            print("Error: \(new_color.rawValue) was already chosen\n")
+        }
+    }
+    
+    // Adds one or many package selections to the object
+    func addPackage(package: [Packages]) {
+        for item in package {
+            if self.package.contains(item) { // catch if a package already on list
+                print("Error: \(item.rawValue) was previously added")
+            } else {
+                billsPrius.package.append(item)
+                print("Added: \(item.rawValue)")
+            }
+        }
+        print("")
+    }
+    
+    // Adds one or many accessory selections to the object
+    func addAccessory(accessory: [Accessories]) {
+        for item in accessory {
+            if self.accessory.contains(item) { // catch if a accessory already on list
+                print("Error: \(item.rawValue) was previously added")
+            } else {
+                billsPrius.accessory.append(item)
+                print("Added: \(item.rawValue)")
+            }
+        }
+        print("")
+    }
+    
+    // Removes a package selection from the object
     func removePackage(package: Packages) {
+        // Displays if package attempting to be removed was not in array
+        if !self.package.contains(package) {
+            print("Error: \"\(package.rawValue)\" was not previously selected")
+        }
         for item in self.package {
             var counter = 0
             if package == item {
-                print("\(item.rawValue) has been removed")
+                print("Removed: \(item.rawValue)")
                 self.package.remove(at: counter)
             }
             counter += 1
@@ -123,11 +184,16 @@ class Prius {
         print("")
     }
     
+    // Removes a accessory selection from the object
     func removeAccessory(accessory: Accessories) {
+        // Displays if accessory attempting to be removed was not in array
+        if !self.accessory.contains(accessory) {
+            print("Error: \"\(accessory.rawValue)\" was not previously selected")
+        }
         for item in self.accessory {
             var counter = 0
             if accessory == item {
-                print("\(item.rawValue) has been removed")
+                print("Removed: \(item.rawValue)")
                 self.accessory.remove(at: counter)
             }
             counter += 1
@@ -136,15 +202,35 @@ class Prius {
     }
 }
 
-var myPrius = Prius(model: .LEco, color: .ClassicSilverMetallic)
+var billsPrius = Prius(model: .NightshadeEditionFWDe, color: .ClassicSilverMetallic)
 
-myPrius.showSelections()
+billsPrius.showSelections()
 
-myPrius.package = [.PreferredAccessoryPackageWithCarpetMats, .AllWeatherFloorLinerPackage, .PremiumConveniencePackage]
-myPrius.accessory = [.BlackoutWheelInserts, .AllWeatherFloorLiner]
+billsPrius.addAccessory(accessory: [.QuickChargeCablePackage])
 
-myPrius.showSelections()
+billsPrius.showSelections()
 
-myPrius.removePackage(package: .PreferredAccessoryPackageWithCarpetMats)
+billsPrius.addPackage(package: [.AdvancedTechnologyPackage, .PremiumConveniencePackage])
 
-myPrius.showSelections()
+billsPrius.showSelections()
+
+billsPrius.removePackage(package: .PremiumConveniencePackage)
+
+billsPrius.showSelections()
+
+billsPrius.removeAccessory(accessory: .QuickChargeCablePackage)
+
+billsPrius.showSelections()
+
+billsPrius.removePackage(package: .ProtectionPackage3)
+billsPrius.removeAccessory(accessory: .UniversalTabletHolder)
+
+billsPrius.addPackage(package: [.PremiumConveniencePackage, .AllWeatherFloorLinerPackage, .CarpetMatPackage])
+billsPrius.addAccessory(accessory: [.UniversalTabletHolder, .CargoTote, .BodySideMoldings])
+billsPrius.changeModel(new_model: .NightshadeEditionFWDe)
+billsPrius.changeModel(new_model: .Limited)
+billsPrius.changeColor(new_color: .ClassicSilverMetallic)
+billsPrius.changeColor(new_color: .MidnightBlackMetallic)
+
+billsPrius.showSelections()
+
